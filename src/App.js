8708d87c07f15047f26c2e8e3fa34b0bf3599c11
rajeevtyagi <rajeev.tyagi@vizexec.com>
2021-06-13 17:25:41 +0530

    changed app text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-       Welcome to react uiuii world !!
+       I was changed on feature branch.
         </p>
         <a
           className="App-link"
